18647358aefee9fd3eecb1daf3e223235171da22
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-05-29 11:54:35 -0300

    chore: using a defaultOption object to set default values

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,50 @@
 import type { Preset, PresetOptions } from "unocss";
 
+const defaultOption: Required<PresetFluidOptions> = {
+  minPortSize: 600,
+  maxPortSize: 1400,
+  minScale: 1.2,
+  maxScale: 1.4,
+  minSize: 16,
+  maxSize: 22,
+};
+
 interface PresetFluidOptions extends PresetOptions {
-  portSize?: [number, number];
-  scale?: [number, number];
-  baseSize?: [number, number];
+  minPortSize?: number;
+  maxPortSize?: number;
+  minScale?: number;
+  maxScale?: number;
+  minSize?: number;
+  maxSize?: number;
 }
 
 export function presetFluid(options: PresetFluidOptions = {}): Preset {
-  options.portSize ??= [600, 1400];
-  options.scale ??= [1.2, 1.25];
-  options.baseSize ??= [16, 22];
+  const config = {
+    ...defaultOption,
+    ...options,
+  };
 
   return {
     name: "unocss-preset-fluid",
     rules: [
       [
         /^fluid-(-?\d+)$/,
-        ([, step]) => ({ "font-size": getFluidSize(Number(step), options) }),
+        ([, step]) => ({ "font-size": getFluidSize(Number(step), config) }),
       ],
     ],
   };
 }
 
-function getFluidSize(
-  step: number,
-  { portSize, scale, baseSize }: PresetFluidOptions
-) {
-  const minFontSize = baseSize[0] * scale[0] ** step;
-  const maxFontSize = baseSize[1] * scale[1] ** step;
+function getFluidSize(step: number, ...config: any) {
+  const minFontSize = minSize * minScale ** step;
+  const maxFontSize = maxSize * maxScale ** step;
 
   // this formula is based on:
   // https://www.smashingmagazine.com/2022/01/modern-fluid-typography-css-clamp/
-  const v = (100 * (maxFontSize - minFontSize)) / (portSize[1] - portSize[0]);
+  const v = (100 * (maxFontSize - minFontSize)) / (maxPortSize - minPortSize);
   const r =
-    (maxFontSize * portSize[0] - minFontSize * portSize[1]) /
-    (portSize[0] - portSize[1]) /
+    (maxFontSize * minPortSize - minFontSize * maxPortSize) /
+    (minPortSize - maxPortSize) /
     16;
 
   return `clamp(${minFontSize / 16}rem, ${v}vw + ${r}rem, ${
